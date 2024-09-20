f3f8436a41d6b6656b9f08042a98414817394b33
Денисова Алиса Евгеньевна <adeavoice@gmail.com>
2024-09-20 14:52:39 +0300

    1.0.7 teams

--- src/components/main/TeamModal.jsx
+++ src/components/main/TeamModal.jsx
@@ -4,19 +4,23 @@ const TeamModal = ({ team, onClose, participants }) => {
   if (!team) return null;
 
   return (
-    <div className="modal-content" isOpen={!!team}>
-      <span className="close" onClick={onClose}>&times;</span>
-      <h2>{team.name}</h2>
-      <p>Количество участников: {team.members}</p>
-      <p>Общий прогресс: {team.totalProgress}</p>
-      <h3>Участники:</h3>
-      <ul>
-        {participants.filter(participant => participant.team === team.name).map(participant => (
-          <li key={participant.id}>
-            {participant.lastName} {participant.firstName} - Прогресс: {participant.progress}
-          </li>
-        ))}
-      </ul>
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={e => e.stopPropagation()}>
+        <span className="close" onClick={onClose}>&times;</span>
+        <h2>{team.name}</h2>
+        <p>Количество участников: {team.members}</p>
+        <p>Общий прогресс: {team.totalProgress}</p>
+        <h3>Участники:</h3>
+        <ul>
+          {participants
+            .filter(participant => participant.team === team.name)
+            .map(participant => (
+              <li key={participant.id}>
+                {participant.lastName} {participant.firstName} - Прогресс: {participant.progress}
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 };
