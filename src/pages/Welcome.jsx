698a43a04435b653f074f4854cded7e161465f8d
Денисова Алиса Евгеньевна <adeavoice@gmail.com>
2025-05-27 12:13:48 +0300

    1.0.72 fix welcome style

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import WelcomeBlock from '../components/WelcomeBlock'
 import FullWidthText from '../components/FullWidthText'
 
 const Welcome = () => {
   const welcomeText =
     'СПОРТИВНЫЙ ЧЕЛЛЕНДЖ ДИТ СПОРТИВНЫЙ ЧЕЛЛЕНДЖ ДИТ СПОРТИВНЫЙ ЧЕЛЛЕНДЖ ДИТ СПОРТИВНЫЙ ЧЕЛЛЕНДЖ ДИТ СПОРТИВНЫЙ ЧЕЛЛЕНДЖ ДИТ СПОРТИВНЫЙ ЧЕЛЛЕНДЖ ДИТ'
+
+  useEffect(() => {
+    const setAppHeight = () => {
+      const appHeight = window.innerHeight
+      document.documentElement.style.setProperty('--app-height', `${appHeight}px`)
+    }
+
+    setAppHeight()
+    window.addEventListener('resize', setAppHeight)
+
+    return () => {
+      window.removeEventListener('resize', setAppHeight)
+    }
+  }, [])
 
   return (
     <div className="welcome">
