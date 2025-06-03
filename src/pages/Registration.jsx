40c2a39cc78244abd0bc0accf1b46fc77746afb4
Денисова Алиса Евгеньевна <adeavoice@gmail.com>
2025-06-03 08:36:18 +0300

    1.0.74 fix height

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import RegistrationBlock from '../components/RegistrationBlock'
 import FullWidthText from '../components/FullWidthText'
 
 const Registration = () => {
   const welcomeText =
     'СПОРТИВНЫЙ ЧЕЛЛЕНДЖ ДИТ СПОРТИВНЫЙ ЧЕЛЛЕНДЖ ДИТ СПОРТИВНЫЙ ЧЕЛЛЕНДЖ ДИТ СПОРТИВНЫЙ ЧЕЛЛЕНДЖ ДИТ СПОРТИВНЫЙ ЧЕЛЛЕНДЖ ДИТ СПОРТИВНЫЙ ЧЕЛЛЕНДЖ ДИТ'
+
+  useEffect(() => {
+    const setAppHeight = () => {
+      const appHeight = window.innerHeight
+      document.documentElement.style.setProperty('--app-height', `${appHeight}px`)
+    }
+
+    setAppHeight()
+    window.addEventListener('resize', setAppHeight)
+
+    return () => {
+      window.removeEventListener('resize', setAppHeight)
+    }
+  }, [])
 
   return (
     <div className="welcome">
